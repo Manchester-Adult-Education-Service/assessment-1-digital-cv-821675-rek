2a8e8df670a7598dd5313c3bca87078ff3806ccb
Rekha Norman <821675@manadulted.ac.uk>
2026-02-26 20:25:41 +0000

    update to CV using JS

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const nameHeading = document.querySelector('h1');
 const hour = new Date().getHours();
 const year = new Date().getFullYear();
 const time = new Date().toLocaleTimeString();
-Document.getElementById('copyright').textContent = Copyright @ ${year} Rekha'
-Document.getElementsById('copyright').textContent = Copyright @ ${time} Rekha'
 
 if (hour<12) {
     nameHeading.textContent = "Good Morning, Rekha";
@@ -15,6 +13,23 @@ if (hour<12) {
 
 console.log("The current hour is: " + hour);
 
-document.getElementById(`copyright @ Rekha`)
-const personalStatement = document.querySelector('highlight')
-personalStatement.style.backgroundColor = '#bfff00'
+document.getElementById('copyright').textContent = `Copyright @ ${year} Rekha`
+
+const personalStatement = document.getElementsByClassName('highlight');
+if (personalStatement && personalStatement.style) {
+    personalStatement.style.backgroundColor = '#bfff00'
+}
+
+alert('Thank you for visiting my portfolio!');
+
+document.querySelectorAll('h1')
+        .forEach(el => el.style.fontSize += 2);
+
+document.getElementById('theme-btn').addEventListener('click', function() { 
+console.log(document.body.style.backgroundColor);
+    if (document.body.style.backgroundColor === 'rgb(30, 27, 75)') {
+        document.body.style.backgroundColor = 'pink';
+    } else {
+        document.body.style.backgroundColor = 'rgb(30, 27, 75)';
+    }
+ });
